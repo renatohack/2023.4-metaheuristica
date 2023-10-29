3d277672ac2cb97a6f5fc71a14beb2dd198b052f
Renato Hack <renato.hack@al.infnet.edu.br>
2023-10-29 11:31:22 -0300

    Testes finalizados

---
+++ 1-teste-aula-3/alimento.py
@@ -0,0 +1,11 @@
+class Alimento():
+    
+    def __init__(self, nome, carb, fibra, prot, calcio, b1, b6, preco):
+        self.nome = nome
+        self.carb = carb
+        self.fibra = fibra
+        self.prot = prot
+        self.calcio = calcio
+        self.b1 = b1
+        self.b6 = b6
+        self.preco = preco
